ce2234f53aab5a92a942b875a0d503a464da6354
javed <javedp3@gmail.com>
2024-08-24 17:26:58 +0530

    new delted commit

--- cluster.crate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
-sudo mv -v minikube /usr/local/bin
-minikube version
-minikube start -p demo-cluster --node 3
